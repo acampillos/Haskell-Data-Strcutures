8f419a9f7bd8401d21100b348e4934f3514fd2fa
acampillos <49621990+acampillos@users.noreply.github.com>
2021-02-01 03:00:45 +0100

    AVLTree and tests.

--- testACD.hs
+++ testACD.hs
@@ -1,16 +1,16 @@
-import Data.List as L
-import DataStructures.BinarySearchTree as BST
+--import Data.List as L
+--import DataStructures.BinarySearchTree as BST
 
 
 ------------------------
 -- TESTS
 ------------------------
+
+-- BST
+{--
 test1, test2 :: BSTree Int
 test1 = foldr BST.insert H [50,40,60,70,80]
 test2 = undefined
-
-
-{--
 ejbt1,ejbt2,ejbt3,ejbt4,ejbt5 :: BSTree Int
 ejbt1 = (N (N (H) (H) 5) (N (N (N (H) (H) 9) (N (H) (H) 13) 12) (N (H) (N (H) (H) 23) 19) 15) 8)
 -- Prueba para ver que no es SBT
@@ -22,3 +22,101 @@ ejbt5 = (N (N (N (H) (H) 1) (N (N (N (H) (H) 122) (N (H) (H) 126) 125) (N (N (N 
 test1, test2 :: BSTree Int
 test1 = (N (N (H) (H) 40) (N (N (H) (H) 60) (N (H) (H) 80) 70) 50)
 test2 = (N (N (H) (H) 40) (N (N (H) (H) 60) (N (H) (H) 80) 70) 50)--}
+
+
+-- AVLTree
+
+data AVLTree a = N a (AVLTree a) (AVLTree a)
+              | H
+              deriving (Show, Eq)
+
+left :: AVLTree a -> AVLTree a
+left H = H
+left (N _ l _) = l
+
+right :: AVLTree a -> AVLTree a
+right H = H
+right (N _ _ r) = r
+
+depth :: AVLTree a -> Int
+depth H = -1
+depth (N _ l r) = 1 + max (depth l) (depth r)
+
+balanced :: AVLTree a -> Bool
+balanced H = True
+balanced (N v l r)
+    | not (balanced r) = False
+    | not (balanced l) = False
+    | bf > 1 = False
+    | otherwise = True
+    where bf = balanceFactor l r
+
+balanceFactor :: AVLTree a -> AVLTree a -> Int
+balanceFactor a b = abs((depth a)-(depth b))
+
+value :: AVLTree a -> a
+value H = error "ERROR"
+value (N v l r) = v
+
+insert :: (Ord a) => AVLTree a -> a -> AVLTree a
+insert H a = N a H H
+insert (N v l r) a
+    | v > a = rotate(N v (insert l a) r)
+    | otherwise =  rotate(N v l (insert r a))
+
+rotate :: AVLTree a -> AVLTree a
+rotate H = H
+rotate (N v l r) | not (balanced l) = N v (rotate l) r
+                 | not (balanced r) = N v l (rotate r)
+                 -- left left
+                 | (depth r) + 1 < (depth r) && (depth (right l)) < (depth (left l)) = N (value l) (left l) (N v (right l) r)
+                 -- left right
+                 | (depth r) + 1 < (depth l) && (depth (right l)) > (depth (left l)) = N (value (right l)) (N (value l) (left l) (left (right l))) (N v (right (right l)) r)
+                 -- right right
+                 | (depth l) + 1 < (depth r) && (depth (left r)) < (depth (right r)) = N (value r) (N v l (left r)) (right r)
+                 -- right left
+                 | (depth l) + 1 < (depth r) && (depth (left r)) > (depth (right r)) = N (value (left r)) (N v l (left (left r))) (N (value r) (right (left r)) (right r))
+                 | otherwise = N v l r
+
+contains :: (Eq a, Ord a) => a -> AVLTree a -> Bool
+contains _ H = False
+contains x (N v l r) = v == x || contains v l || contains v r
+
+depthOf :: (Eq a, Ord a) => a -> AVLTree a -> Int
+depthOf _ H = -1
+depthOf v (N n izq der)
+    | n == v = 0
+    | otherwise = if v < n then 1 + (depthOf v izq) else 1 + (depthOf v der)
+
+
+inorder :: AVLTree a -> [a]
+inorder (N v l r) = (inorder l) ++ [v] ++ (inorder r)
+inorder H = []
+
+preorder :: AVLTree a -> [a]
+preorder (N v l r) = [v] ++ (preorder l) ++ (preorder r)
+preorder H = []
+
+postorder :: AVLTree a -> [a]
+postorder (N v l r) = (postorder l) ++ (postorder r) ++ [v]
+postorder H = []
+
+equals :: (Eq a, Ord a) => AVLTree a -> AVLTree a -> Bool
+equals (N a l1 r1) (N b l2 r2) = a==b && equals l1 l2 && equals r1 r2
+equals (N _ _ _) H = False
+equals H (N _ _ _) = False
+equals H H = True
+
+test1, test2, test3 :: AVLTree Int
+test1 = (N 3 (N 2 (N 1 H H) H) H)
+test2 = (N 4 (N 7 (N 21 H H) (N 12 H H)) (N 15 H H))
+test3 = H
+
+
+------------------------
+------------------------
+-- SORTING
+------------------------
+------------------------
+
+quicksort :: (Ord a)= > 
