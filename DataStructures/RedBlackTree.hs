f05c660172a711ad2212eaa07672b634bfc9c968
marespcue1 <57679059+marespcue1@users.noreply.github.com>
2021-02-01 14:51:17 +0100

    Insertar en Red Black Tree

--- DataStructures/RedBlackTree.hs
+++ DataStructures/RedBlackTree.hs
@@ -1,48 +1,50 @@
 module DataStructures.RedBlackTree(
     RBTree(..),
+    Color(..),
     depthRBT,
     blackDepth,
     getKeyRBT,
     isBlackRBT,
+    makeBlack,
     countBlack,
     allRedHasBlackChilds,
     isBST_RBT,
     isRBT,
     depthRBTOf,
     minRBT,
-    maxRBT
+    maxRBT,
+    insertRBT,
+    balanceRBT
 ) where
-
-data RBTree a = B a (RBTree a) (RBTree a)
-             | R a (RBTree a) (RBTree a)
-             | L
-             deriving (Show, Eq)
+data Color = R | B deriving (Show, Eq)
+data RBTree a = N Color a (RBTree a) (RBTree a)
+              | L
+              deriving (Show, Eq)
 
 
 -- Comprobar depth
 depthRBT :: (Eq a, Ord a) => RBTree a -> Int
 depthRBT L = 0
-depthRBT (B _ lef rig) = 1 + max (depthRBT lef) (depthRBT rig)
-depthRBT (R _ lef rig) = 1 + max (depthRBT lef) (depthRBT rig)
+depthRBT (N _ _ lef rig) = 1 + max (depthRBT lef) (depthRBT rig)
 
 -- Comprobar blackDepth
 blackDepth :: (Eq a, Ord a) => RBTree a -> Int
 blackDepth L = 0
-blackDepth (B _ lef _) = 1 + (depthRBT lef)
-blackDepth (R _ lef _) = (depthRBT lef)
+blackDepth (N B _ lef _) = 1 + (blackDepth lef)
+blackDepth (N R _ lef _) = (blackDepth lef)
 
 -- Sacar el valor
 getKeyRBT :: (Eq a, Ord a) => RBTree a -> Maybe a
 getKeyRBT (L) = Nothing
-getKeyRBT (B k _ _) = Just k
-getKeyRBT (R k _ _) = Just k
+getKeyRBT (N _ k _ _) = Just k
 
 -- Es negro?
 isBlackRBT :: (Eq a, Ord a) => RBTree a -> Bool
-isBlackRBT (L) = True
-isBlackRBT (B _ _ _) = True
-isBlackRBT (R _ _ _) = False
+isBlackRBT (N R _ _ _) = False
+isBlackRBT _ = True
 
+makeBlack :: (Eq a, Ord a) => RBTree a -> RBTree a
+makeBlack (N _ n lef rig) = (N B n lef rig)
 --------------------------------------
 -- Comprobando si se trata de un RBT--
 --------------------------------------
@@ -58,34 +60,22 @@ isBlackRBT (R _ _ _) = False
 -- Comprueba si hay el mismo número de nodos negros en todos los caminos simples hasta las hojas
 countBlack :: (Eq a, Ord a) => RBTree a -> Maybe Int
 countBlack (L) = Just 0
-countBlack (B _ lef rig)
+countBlack (N color _ lef rig)
     | countBlack lef == Nothing || countBlack rig == Nothing = Nothing
+    | color == R = if lefCount == rigCount then Just (lefCount) else Nothing
     | otherwise = if lefCount == rigCount then Just (1 + lefCount) else Nothing
-    where Just lefCount = countBlack lef
-          Just rigCount = countBlack rig
-countBlack (R _ lef rig)
-    | countBlack lef == Nothing || countBlack rig == Nothing = Nothing
-    | otherwise = if lefCount == rigCount then Just (lefCount) else Nothing
     where Just lefCount = countBlack lef
           Just rigCount = countBlack rig
 
 -- Comprobar si todos los rojos tienen hijos negros
 allRedHasBlackChilds :: (Eq a, Ord a) => RBTree a -> Bool
 allRedHasBlackChilds (L) = True
-allRedHasBlackChilds (B _ lef rig) = allRedHasBlackChilds lef && allRedHasBlackChilds rig
-allRedHasBlackChilds (R _ lef rig) = if isBlackRBT lef && isBlackRBT rig then allRedHasBlackChilds lef && allRedHasBlackChilds rig else False
+allRedHasBlackChilds (N B _ lef rig) = allRedHasBlackChilds lef && allRedHasBlackChilds rig
+allRedHasBlackChilds (N R _ lef rig) = if isBlackRBT lef && isBlackRBT rig then allRedHasBlackChilds lef && allRedHasBlackChilds rig else False
 
 -- Comprobar si es Binario
 isBST_RBT :: (Eq a, Ord a) => RBTree a -> Bool
-isBST_RBT (B n izq der)
-    | getKeyRBT izq == getKeyRBT der && getKeyRBT der == Nothing = True
-    | getKeyRBT izq == Nothing = (valD > n) && isBST_RBT der
-    | getKeyRBT der == Nothing = (valI < n) && isBST_RBT izq
-    | otherwise = (valD > n) && (valI < n) && isBST_RBT der && isBST_RBT izq
-    where
-        Just valI = getKeyRBT izq
-        Just valD = getKeyRBT der
-isBST_RBT (R n izq der)
+isBST_RBT (N _ n izq der)
     | getKeyRBT izq == getKeyRBT der && getKeyRBT der == Nothing = True
     | getKeyRBT izq == Nothing = (valD > n) && isBST_RBT der
     | getKeyRBT der == Nothing = (valI < n) && isBST_RBT izq
@@ -98,7 +88,7 @@ isBST_RBT (L) = True
 -- Comprueba si cumple las propiedades de un RBT
 isRBT :: (Eq a, Ord a) => RBTree a -> Bool
 isRBT L = True
-isRBT (R _ _ _) = False
+isRBT (N R _ _ _) = False
 isRBT t = allRedHasBlackChilds t && isBST_RBT t && sameBlackPath
     where sameBlackPath = if countBlack t == Nothing then False else True
 
@@ -108,36 +98,26 @@ isRBT t = allRedHasBlackChilds t && isBST_RBT t && sameBlackPath
 -- Da la profundidad de un elemento, si no está da -1
 depthRBTOf :: (Eq a, Ord a) => a -> RBTree a -> Int
 depthRBTOf _ L = -1
-depthRBTOf v (B n izq der)
+depthRBTOf v (N _ n izq der)
     | n == v = 0
     | otherwise = if v < n then 1 + valueOfDepthLef else 1 + valueOfDepthRig
     where depthLef = (depthRBTOf v izq) 
           depthRig = (depthRBTOf v der)
           valueOfDepthLef = if 0 > depthLef then -2 else depthLef
           valueOfDepthRig = if 0 > depthRig then -2 else depthRig
-depthRBTOf v (R n izq der)
-    | n == v = 0
-    | otherwise = if v < n then 1 + valueOfDepthLef else 1 + valueOfDepthRig
-    where depthLef = (depthRBTOf v izq) 
-          depthRig = (depthRBTOf v der)
-          valueOfDepthLef = if 0 > depthLef then -2 else depthLef
-          valueOfDepthRig = if 0 > depthRig then -2 else depthRig
+
 
 -- Elemento minimo del arbol
 minRBT :: (Eq a, Ord a) => RBTree a -> Maybe a
 minRBT (L) = Nothing
-minRBT (B n (L) _) = Just n
-minRBT (B _ lef _) = minRBT lef
-minRBT (R n (L) _) = Just n
-minRBT (R _ lef _) = minRBT lef 
+minRBT (N _ n (L) _) = Just n
+minRBT (N _ _ lef _) = minRBT lef
 
 -- Elemento maximo del arbol
 maxRBT :: (Eq a, Ord a) => RBTree a -> Maybe a
 maxRBT (L) = Nothing
-maxRBT (B n _ (L)) = Just n
-maxRBT (B _ _ rig) = maxRBT rig
-maxRBT (R n _ (L)) = Just n
-maxRBT (R _ _ rig) = maxRBT rig 
+maxRBT (N _ n _ (L)) = Just n
+maxRBT (N _ _ _ rig) = maxRBT rig
 
 -----------
 --Updates--
@@ -152,3 +132,23 @@ data Side = LEFT | RIGHT
 rotation :: (Eq a, Ord a) => a -> Side -> RBTree a -> RBTree a
 rotation v LEFT t = undefined
 rotation v RIGHT t = undefined
+
+-- Insertar elemento en Red Black Tree
+insertRBT :: (Eq a, Ord a) => a -> RBTree a -> RBTree a
+insertRBT x s = makeBlack $ ins s
+  where ins (L) = N R x (L) (L)
+        ins (N color y a b)
+          | x < y  = balanceRBT color y (ins a) b
+          | x == y = N color y a b
+          | x > y  = balanceRBT color y a (ins b)
+
+-- Hace las rotaciones del Red Black Tree
+balanceRBT :: Color -> a -> RBTree a -> RBTree a -> RBTree a
+balanceRBT B z (N R y (N R x a b) c) d = N R y (N B x a b) (N B z c d)
+balanceRBT B z (N R x a (N R y b c)) d = N R y (N B x a b) (N B z c d)
+balanceRBT B x a (N R z (N R y b c) d) = N R y (N B x a b) (N B z c d)
+balanceRBT B x a (N R y b (N R z c d)) = N R y (N B x a b) (N B z c d)
+balanceRBT color x a b = N color x a b
+
+-- Da el nodo padre de un nodo
+{-parent :: (Eq a, Ord a) => RBTree -> RBTree a -> -}
--- testMEC.hs
+++ testMEC.hs
@@ -1,18 +1,18 @@
 import Data.List as L
-import DataStructures.BinarySearchTree as BST
+--import DataStructures.BinarySearchTree as BST
 import DataStructures.RedBlackTree as RBT
 -- Zona de tests para marespecue1
 
 
 ejbrt1,ejbrt2,ejbrt3,ejbrt4,ejbrt5 :: RBTree Int
-ejbrt1 = (B 7 (B 3 (L) (L)) (R 18 (B 10 (R 8 (L) (L)) (R 11 (L) (L))) (B 22 (L) (R 26 (L) (L)))))
+ejbrt1 = (N B 7 (N B 3 (L) (L)) (N R 18 (N B 10 (N R 8 (L) (L)) (N R 11 (L) (L))) (N B 22 (L) (N R 26 (L) (L)))))
 -- Ejemplos de RBT no valido
 -- No tiene todos los caminos hasta las hojas con misma cantidad de negros
-ejbrt2 = (B 7 (B 3 (L) (L)) (R 18 (B 10 (R 8 (L) (L)) (R 11 (L) (L))) (B 22 (L) (R 26 (B 25 (L) (L)) (L)))))
+ejbrt2 = (N B 7 (N B 3 (L) (L)) (N R 18 (N B 10 (N R 8 (L) (L)) (N R 11 (L) (L))) (N B 22 (L) (N R 26 (N B 25 (L) (L)) (L)))))
 -- La raiz es roja
-ejbrt3 = (R 7 (B 3 (L) (L)) (R 18 (B 10 (R 8 (L) (L)) (R 11 (L) (L))) (B 22 (L) (R 26 (B 25 (L) (L)) (L)))))
+ejbrt3 = (N R 7 (N B 3 (L) (L)) (N R 18 (N B 10 (N R 8 (L) (L)) (N R 11 (L) (L))) (N B 22 (L) (N R 26 (N B 25 (L) (L)) (L)))))
 -- Un rojo tiene un hijo rojo
-ejbrt4 = (B 7 (B 3 (L) (L)) (R 18 (R 10 (R 8 (L) (L)) (R 11 (L) (L))) (B 22 (L) (R 26 (B 25 (L) (L)) (L)))))
+ejbrt4 = (N B 7 (N B 3 (L) (L)) (N R 18 (N R 10 (N R 8 (L) (L)) (N R 11 (L) (L))) (N B 22 (L) (N R 26 (N B 25 (L) (L)) (L)))))
 -- No es BST
-ejbrt5 = (B 7 (B 42 (L) (L)) (R 18 (R 10 (R 8 (L) (L)) (R 11 (L) (L))) (B 22 (L) (R 26 (B 25 (L) (L)) (L)))))
+ejbrt5 = (N B 7 (N B 42 (L) (L)) (N R 18 (N R 10 (N R 8 (L) (L)) (N R 11 (L) (L))) (N B 22 (L) (N R 26 (N B 25 (L) (L)) (L)))))
 
