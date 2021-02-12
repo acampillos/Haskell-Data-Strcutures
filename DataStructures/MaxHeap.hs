f957e2b1f8f43de9c993ee37569a9b3ac56b3399
acampillos <49621990+acampillos@users.noreply.github.com>
2021-02-12 22:29:43 +0100

    Comments and refactoring on heaps.

--- DataStructures/MaxHeap.hs
+++ DataStructures/MaxHeap.hs
@@ -1,21 +1,36 @@
+module DataStructures.MaxHeap(
+    MaxHeap(..),
+    empty,
+    isEmpty,
+    DataStructures.MaxHeap.insert,
+    merge,
+    findMax,
+    deleteMax,
+    elements,
+    equals,
+    rank
+) where
+
 import Data.List
--- LEFTIST HEAP
--- empty
--- isEmpty
--- insert
--- merge
--- findMin
--- deleteMin
 
--- elements
--- equals
+-- MONTÍCULO BINARIO MÁXIMO / MAX BINARY HEAP:
+-- Son árboles binarios completos que cumplen la propiedad de que para cada padre en el árbol
+-- sus hijos tienen un valor menor que el de este. Implementa el ADT de una cola de prioridad.
 
--- TODO
--- 
+-----------------------
+-- CONSTRUCTORES
+-----------------------
 
---                           rango val izq      der
+-- Lo implementamos mediante un árbol binario que en cada nodo también contendrá el rango del mismo.
+-- Entendemos por rango la menor distancia desde el nodo a una hoja del árbol binario.
+-- Lo almacenamos con el fin de ahorrar complejidad temporal a coste de complejidad espacial.
+
 data MaxHeap a = Empty | Tree Int a (MaxHeap a) (MaxHeap a)
     deriving (Show, Eq)
+
+-----------------------
+-- FUNCIONES
+-----------------------
 
 empty :: Ord a => MaxHeap a
 empty = Empty
@@ -25,24 +40,38 @@ isEmpty Empty = True
 isEmpty (Tree _ _ _ _) = False
 
 insert :: (Ord a) => MaxHeap a -> a -> MaxHeap a
+-- Inserta un elemento en el montículo mediante la mezcla del mismo con uno que solo tiene el elemento.
+-- Parámetros: Montículo
+--             Elemento
+-- Devuelve:   Montículo con el elemento
 insert Empty x = Tree 1 x Empty Empty
--- Suponemos un arbol con el valor a insertar y lo combinamos con el existente
 insert t x = merge t (Tree 1 x Empty Empty)
 
 merge :: (Ord a) => MaxHeap a -> MaxHeap a -> MaxHeap a
+-- Mezcla dos montículos.
+-- Parámetros: Montículo 1
+--             Montículo 2
+-- Devuelve:   Montículo mezclando ambos
 merge a Empty = a
 merge Empty a = a
+-- Crea un montículo cuya raíz será el menor elemento entre las raíces de los dos montículos que recibimos
 merge h1@(Tree r1 x a1 b1) h2@(Tree r2 y a2 b2)
-    | x>=y = makeT x (merge h2 a1) b1
-    | otherwise = makeT y (merge h1 a2) b2
+    -- Como hijo izquierdo tendremos el hijo izquierdo del montículo con menor raíz
+    | x>=y = merge' x (merge h2 a1) b1
+    -- Como hijo derecho tendremos la mezcla del hijo derecho del montículo con menor raíz y el montículo completo con mayor raíz
+    | otherwise = merge' y (merge h1 a2) b2
 
--- Hace que el monticulo sea leftist
--- En el caso de que la parte derecha sea mas larga (rango derecho mas grande), intercambia ambos arboles y aumenta el antiguo rango izquierdo debido a x
--- En el caso de que la parte izquierda sea mas larga (rango izq mas grande), intercambia ambos arboles y aumenta el antigo rango derecho 
-makeT :: a -> MaxHeap a -> MaxHeap a -> MaxHeap a
-makeT v Empty b = Tree 0 v b Empty
-makeT v a Empty = Tree 0 v a Empty
-makeT v h1 h2
+merge' :: a -> MaxHeap a -> MaxHeap a -> MaxHeap a
+-- Coloca un elemento como raíz ajustando su rango en función de sus hijos que serán los dos montículos recibidos.
+-- Parámetros: Elemento
+--             Monticulo 1
+--             Monticulo 2
+-- Devuelve:   Monticulo con rango ajustado y con el elemento como raíz
+merge' v Empty b = Tree 1 v b Empty
+merge' v a Empty = Tree 1 v a Empty
+merge' v h1 h2
+    -- El rango del nuevo montículo que será el menor + 1 por el nuevo nodo añadido
+    -- El montículo será leftist (acumulamos los subárboles de mayor tamaño en la parte izquierda del nuevo montículo) 
     | r1 >= r2 = Tree (r2 + 1) v h1 h2
     | otherwise = Tree (r1 + 1) v h2 h1 
     where r1 = rank h1
